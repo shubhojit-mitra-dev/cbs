7366127e02008dc79a2c6dd906c6633bae252817
Pradip Chaudhary <88336529+thepradipvc@users.noreply.github.com>
2024-11-09 11:35:29 +0000

    setup shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
-
+import Link from "next/link";
+import { Button } from "~/components/ui/button";
 import { HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        Hello
+        <Button asChild>
+          <Link href="/api/auth/login">
+            Login
+          </Link>
+        </Button>
       </main>
     </HydrateClient>
   );

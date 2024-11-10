874639f38bf23939bbc4ae878620dbecd33669ae
Pradip Chaudhary <88336529+thepradipvc@users.noreply.github.com>
2024-11-10 04:55:13 +0000

    fixed build errors

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,7 +17,9 @@ const config = {
     "@typescript-eslint/prefer-nullish-coalescing": "off",
     "@typescript-eslint/no-unsafe-assignment": "off",
     "@typescript-eslint/array-type": "off",
+    "@typescript-eslint/consistent-indexed-object-style": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
+    "@typescript-eslint/no-floating-promises": "off",
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
